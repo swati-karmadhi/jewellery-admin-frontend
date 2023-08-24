5ca4e1953cef14c61d63775303ef489e15830d38
Nirav Chalala <nirav@karmadhi.com>
2023-08-24 11:22:09 +0530

    some image related changes in shape master details

--- src/pages/Master/Diamond/Shape/ShapeMasterDetails.jsx
+++ src/pages/Master/Diamond/Shape/ShapeMasterDetails.jsx
@@ -1,12 +1,12 @@
 import styled from "@emotion/styled";
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, TextareaAutosize } from "@mui/material";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Icon, IconButton, TextField, TextareaAutosize } from "@mui/material";
 import React, { useEffect } from "react";
 import * as Yup from "yup";
 import { API, HELPER } from "../../../../services";
 import { apiEndPoint } from "../../../../constants/routesList";
 import { useFormik } from "formik";
 import { NumericFormat } from "react-number-format";
-import { values } from "lodash";
+import * as CONFIG from "../../../../constants/config";
 
 // inital data
 const initialValues = {
@@ -39,7 +39,7 @@ const validationSchema = Yup.object().shape({
 				return isValid;
 			},
 		})
-		.test("FILE_SIZE", "Uploaded file is too big.", (value) => !value || (value && value.size <= 1000000)),
+		.test("FILE_SIZE", "Max file size 1MB", (value) => !value || (value && value.size <= 1048576)),
 });
 
 const StyledTextarea = styled(TextareaAutosize)(
@@ -167,28 +167,66 @@ const ShapeMasterDetails = ({ open, togglePopup, shapeData }) => {
 						value={formikProps.values.description}
 						onChange={formikProps.handleChange}
 					/>
-					<TextField
-						fullWidth={true}
-						size="small"
-						type="file"
-						name="image"
-						inputProps={{ accept: "image/*" }}
-						sx={{ mt: 1, mb: 2 }}
-						onChange={(e) => {
-							formikProps.setFieldValue("image", e.currentTarget.files[0]);
-						}}
-						onBlur={formikProps.handleBlur}
-						helperText={formikProps.touched.image && formikProps.errors.image}
-						error={Boolean(formikProps.errors.image && formikProps.touched.image)}
-					/>
-					<DialogActions>
-						<Button variant="outlined" color="secondary" onClick={togglePopup}>
-							Cancel
-						</Button>
-
-						<Button type="submit" color="primary">
-							Save
-						</Button>
+					<DialogActions sx={{ justifyContent: "space-between", pl: 0 }}>
+						<Box sx={{ display: "flex", alignContent: "center", flexWrap: "unset" }}>
+							<TextField
+								id="icon-button-file"
+								fullWidth={true}
+								size="small"
+								type="file"
+								name="image"
+								inputProps={{ accept: "image/*" }}
+								sx={{ mt: 1, mb: 2, display: "none" }}
+								onChange={(e) => {
+									formikProps.setFieldValue("image", e.currentTarget.files[0]);
+								}}
+								onBlur={formikProps.handleBlur}
+								helperText={formikProps.touched.image && formikProps.errors.image}
+								error={Boolean(formikProps.errors.image && formikProps.touched.image)}
+							/>
+							<label htmlFor="icon-button-file">
+								<IconButton
+									color="primary"
+									id="image"
+									component="span"
+									className="button"
+									onBlur={formikProps.handleBlur}
+									aria-label="Upload picture"
+									disableRipple={true}
+								>
+									{formikProps.values.image && formikProps.values.image !== null ? (
+										<Box
+											id="image"
+											component="img"
+											sx={{
+												height: 50,
+												width: 50,
+												maxHeight: { xs: 25, md: 50 },
+												maxWidth: { xs: 25, md: 50 },
+												...(Boolean(formikProps.errors.image && formikProps.touched.image) && {
+													border: "2px solid #FF3D57",
+												}),
+											}}
+											src={URL.createObjectURL(formikProps.values.image)}
+											// alt={<Icon>photo_camera</Icon>}
+										/>
+									) : (
+										<Icon>photo_camera</Icon>
+									)}
+								</IconButton>
+							</label>
+							{Boolean(formikProps.errors.image && formikProps.touched.image) && (
+								<p style={{ color: "#FF3D57", fontSize: "12px" }}>{formikProps.touched.image && formikProps.errors.image}</p>
+							)}
+						</Box>
+						<Box>
+							<Button variant="outlined" color="secondary" onClick={togglePopup}>
+								Cancel
+							</Button>
+							<Button type="submit" color="primary">
+								Save
+							</Button>
+						</Box>
 					</DialogActions>
 				</form>
 			</DialogContent>
