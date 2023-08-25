a4e7a8306728d3847bb5244f6c4d9a10bfb8dc66
Nirav Chalala <nirav@karmadhi.com>
2023-08-25 18:21:26 +0530

    user list search

--- src/pages/Master/User/User/UserMaster.jsx
+++ src/pages/Master/User/User/UserMaster.jsx
@@ -1,20 +1,58 @@
 import React, { useEffect, useState } from "react";
-import { Box, Icon, IconButton, TableBody, TableCell, TableHead, TablePagination, TableRow, Tooltip } from "@mui/material";
+import {
+	Box,
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	FormControlLabel,
+	Icon,
+	IconButton,
+	Radio,
+	RadioGroup,
+	TableBody,
+	TableCell,
+	TableHead,
+	TablePagination,
+	TableRow,
+	TextField,
+	Tooltip,
+} from "@mui/material";
 import { Breadcrumb, Container, StyledAddButton, StyledTable } from "../../../../components";
 import { apiEndPoint, pageRoutes } from "../../../../constants/routesList";
 import { API, HELPER } from "../../../../services";
 import * as CONFIG from "../../../../constants/config";
 import { useNavigate } from "react-router-dom";
 import UserMasterDetails from "./UserMasterDetails";
+import styled from "@emotion/styled";
+import { green, red } from "@mui/material/colors";
+
+const GreenRadio = styled(Radio)(() => ({
+	color: green[400],
+	"&$checked": { color: green[600] },
+}));
+
+const RedRadio = styled(Radio)(() => ({
+	color: red[400],
+	"&$checked": { color: red[600] },
+}));
+
+const initialData = {
+	searchTxt: "",
+	isActive: "",
+};
 
 const UserMaster = () => {
 	const [page, setPage] = useState(0);
 	const [rowsPerPage, setRowsPerPage] = useState(8);
+	const [searchParam, setSearchParam] = useState({ ...initialData });
 	const [tableDataCount, setTableDataCount] = useState(0);
 	const [tableData, setTableData] = useState([]);
 	const [open, setOpen] = useState(false);
+	const [openSearch, setOpenSearch] = useState(false);
 	const [selectedUserData, setSelectedUserData] = useState(null);
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 	const url = apiEndPoint.user;
 
 	const handleChangePage = (_, newPage) => {
@@ -28,8 +66,8 @@ const UserMaster = () => {
 		getTableData(0, +event.target.value);
 	};
 
-	const getTableData = (pg = page, rpp = rowsPerPage) => {
-		API.get(url, { page: pg, rowsPerPage: rpp }).then((response) => {
+	const getTableData = (pg = page, rpp = rowsPerPage, sp= searchParam) => {
+		API.get(url, { page: pg, rowsPerPage: rpp, ...sp }).then((response) => {
 			setTableDataCount(response.count);
 			setTableData(response.rows);
 		});
@@ -39,12 +77,23 @@ const UserMaster = () => {
 		getTableData();
 	}, []);
 
+	const search = () => {
+		setPage(0);
+		setRowsPerPage(8);
+		getTableData(0, 8);
+		togglePopupSearch();
+	};
+
 	const togglePopup = () => {
 		if (open) {
 			getTableData();
 			setSelectedUserData(null);
 		}
 		setOpen(!open);
+	};
+
+	const togglePopupSearch = () => {
+		setOpenSearch(!openSearch);
 	};
 
 	const handleToggle = (id) => {
@@ -72,15 +121,21 @@ const UserMaster = () => {
 		});
 	};
 
+    const resetSearchForm = ()=>{
+        setSearchParam({...initialData})
+		getTableData(page, rowsPerPage, {...initialData});
+		togglePopupSearch();
+    }
+
 	return (
 		<Container>
-			<Box className="breadcrumb">
-				<Breadcrumb
-					routeSegments={[
-						{ name: "Masters", path: pageRoutes.master.user.user },
-						{ name: "User" },
-					]}
-				/>
+			<Box className="breadcrumb" sx={{ display: "flex", justifyContent: "space-between" }}>
+				<Breadcrumb routeSegments={[{ name: "Masters", path: pageRoutes.master.user.user }, { name: "User" }]} />
+				<Tooltip title="Filter">
+					<IconButton color="inherit" className="button" aria-label="Filter" onClick={togglePopupSearch}>
+						<Icon>filter_list</Icon>
+					</IconButton>
+				</Tooltip>
 			</Box>
 			<Box width="100%" overflow="auto">
 				<StyledTable>
@@ -132,7 +187,7 @@ const UserMaster = () => {
 									)}
 								</TableCell>
 								<TableCell align="center">
-									<IconButton onClick={(e) => navigate(`${pageRoutes.master.user.userPermissions.split(':')[0]}${row.id}`)}>
+									<IconButton onClick={(e) => navigate(`${pageRoutes.master.user.userPermissions.split(":")[0]}${row.id}`)}>
 										<Icon color="warning">fingerprint</Icon>
 									</IconButton>
 									<IconButton onClick={(e) => handleEdit(row)}>
@@ -164,6 +219,41 @@ const UserMaster = () => {
 					<Icon>add</Icon>
 				</StyledAddButton>
 			</Tooltip>
+			<Dialog open={openSearch} onClose={togglePopupSearch} aria-labelledby="form-dialog-title">
+				<DialogTitle id="form-dialog-title">Search Filter</DialogTitle>
+				<DialogContent>
+					<TextField
+						fullWidth={true}
+						size="small"
+						type="text"
+						name="searchTxt"
+						label="Search Text"
+						variant="outlined"
+						value={searchParam.searchTxt}
+						onChange={(e) => setSearchParam((data) => ({ ...data, searchTxt: e.target.value }))}
+						sx={{ mb: 2, mt: 1 }}
+					/>
+					<RadioGroup
+						row
+						aria-label="position"
+						name="isActive"
+						value={searchParam.isActive}
+						onChange={(e) => setSearchParam((data) => ({ ...data, isActive: e.target.value }))}
+					>
+						<FormControlLabel value="" label="All" labelPlacement="start" control={<Radio color="default" />} />
+						<FormControlLabel value="1" label="Active" labelPlacement="start" control={<Radio color="success" />} />
+						<FormControlLabel value="0" label="Inactive" labelPlacement="start" control={<Radio color="error" />} />
+					</RadioGroup>
+				</DialogContent>
+				<DialogActions sx={{ px: 3, pb: 2 }}>
+					<Button variant="outlined" color="secondary" onClick={resetSearchForm}>
+						Reset
+					</Button>
+					<Button type="submit" color="primary" onClick={search}>
+						Search
+					</Button>
+				</DialogActions>
+			</Dialog>
 			<UserMasterDetails open={open} togglePopup={togglePopup} userData={selectedUserData} />
 		</Container>
 	);
