c7dee6c215f3e78008a6e4204814003b664dadba
Nirav Chalala <nirav@karmadhi.com>
2023-08-26 09:10:26 +0530

    auth changes

--- src/auth/AuthGuard.jsx
+++ src/auth/AuthGuard.jsx
@@ -1,12 +1,14 @@
 import { pageRoutes } from '../constants/routesList';
 import useAuth from '../hooks/useAuth';
 import { Navigate, useLocation } from 'react-router-dom';
+import AuthStorage from '../utils/authStorage';
 
 const AuthGuard = ({ children }) => {
   const { isAuthenticated } = useAuth();
+  const tokenFound = AuthStorage.isUserAuthenticated();
   const { pathname } = useLocation();
 
-  if (isAuthenticated) return <>{children}</>;
+  if (isAuthenticated && tokenFound) return <>{children}</>;
 
   return <Navigate replace to={pageRoutes.general.login} state={{ from: pathname }} />;
 };
